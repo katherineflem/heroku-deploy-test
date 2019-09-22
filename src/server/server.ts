bff3a90292e08a6b7a0caadcdb8789db1ed870bb
Katherine Fleming <katherinemarief@gmail.com>
2019-09-22 10:54:22 -0500

    ready to go

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,9 +8,11 @@ let p = path.join(__dirname, '../public');
 console.log(p);
 
 app.use(express.static(p));
-app.use(apiRouter);
+app.use(apiRouter); //dont really need this for heroku deploy
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
 });
+
+//at bare minimum you have to have a backend server listening on the port we have assigned and serve up a static file
